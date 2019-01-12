5ed528be518a5cdf0dcb1d6932d0b3dde38cc39d
luizcbfreitas <luizcbfreitas@gmail.com>
2019-01-12 16:23:37 -0200

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/luizcbfreitas/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.luizcbfreitas.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luizcbfreitas.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+	
+	
+
+}
