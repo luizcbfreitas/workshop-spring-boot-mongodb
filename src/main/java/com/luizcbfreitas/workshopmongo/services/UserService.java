2b9222f0c8735945b98d85c055e279855245a0e0
luizcbfreitas <luizcbfreitas@gmail.com>
2019-01-14 13:59:11 -0200

    Inserção de usuário com POST

--- src/main/java/com/luizcbfreitas/workshopmongo/services/UserService.java
+++ src/main/java/com/luizcbfreitas/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luizcbfreitas.workshopmongo.domain.User;
+import com.luizcbfreitas.workshopmongo.dto.UserDTO;
 import com.luizcbfreitas.workshopmongo.repository.UserRepository;
 import com.luizcbfreitas.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,6 +25,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
 
 }
